7f7a70487f79530a1128b60ce7e3a4cc3eff67a5
wgoins6 <wgoins6@gatech.edu>
2022-12-24 15:55:06 -0500

    Day 5 - Processed table into arrays

---
+++ Day5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+string[] fileContents = System.IO.File.ReadAllLines("input.txt");
+
+ArrayList stackList = new ArrayList();
+ArrayList stackArrayList = new ArrayList();
+bool finishedTable = false;
+bool skipLine = false;
+bool isFirstPass = true;
+foreach (string line in fileContents)
+{
+    if (skipLine)
+    {
+        skipLine = false;
+        continue;
+    }
+
+    Console.WriteLine(line);
+    if (char.IsDigit(line[1]))
+    {
+        finishedTable = true;
+        skipLine = true;
+        continue;
+    }
+    if (!finishedTable)
+    {
+        for (int i=0; i < line.Length; i+=4)
+        {
+            string box = line.Substring(i, 3);
+            Console.WriteLine(box);
+            if (isFirstPass)
+            {
+                stackArrayList.Add(box);
+            }
+            else
+            {
+                stackArrayList[i / 4] += box;
+            }
+            // Console.ReadLine();
+        }
+        isFirstPass = false;
+    }
+    // Console.ReadLine();
+}
