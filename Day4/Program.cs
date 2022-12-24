046db303eb3aeb998cbaf56827fdba27a897839e
wgoins6 <wgoins6@gatech.edu>
2022-12-23 22:46:16 -0500

    Completed Day 4

---
+++ Day4/Program.cs
@@ -0,0 +1,78 @@
+﻿static void part1()
+{
+    string[] fileContents = System.IO.File.ReadAllLines("input.txt");
+
+    int numOverlappingPairs = 0;
+    foreach (string line in fileContents)
+    {
+        Console.WriteLine(line);
+        string[] assignments = line.Split(",");
+        string assignmentOne = assignments[0];
+        string assignmentTwo = assignments[1];
+
+        string[] assignmentOneSections = assignmentOne.Split("-");
+        int assignmentOne_One = int.Parse(assignmentOneSections[0]);
+        int assignmentOne_Two = int.Parse(assignmentOneSections[1]);
+
+        string[] assignmentTwoSections = assignmentTwo.Split("-");
+        int assignmentTwo_One = int.Parse(assignmentTwoSections[0]);
+        int assignmentTwo_Two = int.Parse(assignmentTwoSections[1]);
+
+        if (assignmentOne_One >= assignmentTwo_One && assignmentOne_Two <= assignmentTwo_Two)
+        {
+            numOverlappingPairs += 1;
+        }
+        else if (assignmentTwo_One >= assignmentOne_One && assignmentTwo_Two <= assignmentOne_Two)
+        {
+            numOverlappingPairs += 1;
+        }
+
+        // Console.ReadLine();
+    }
+
+    Console.WriteLine(numOverlappingPairs);
+}
+
+static void part2()
+{
+    string[] fileContents = System.IO.File.ReadAllLines("input.txt");
+
+    int numOverlappingPairs = 0;
+    foreach (string line in fileContents)
+    {
+        // Console.WriteLine(line);
+        string[] assignments = line.Split(",");
+        string assignmentOne = assignments[0];
+        string assignmentTwo = assignments[1];
+
+        string[] assignmentOneSections = assignmentOne.Split("-");
+        int assignmentOne_One = int.Parse(assignmentOneSections[0]);
+        int assignmentOne_Two = int.Parse(assignmentOneSections[1]);
+
+        string[] assignmentTwoSections = assignmentTwo.Split("-");
+        int assignmentTwo_One = int.Parse(assignmentTwoSections[0]);
+        int assignmentTwo_Two = int.Parse(assignmentTwoSections[1]);
+
+        if (assignmentOne_One >= assignmentTwo_One && assignmentOne_One <= assignmentTwo_Two)
+        {
+            // Console.WriteLine("Overlap1");
+            numOverlappingPairs += 1;
+        }
+        else if (assignmentOne_Two >= assignmentTwo_One && assignmentOne_Two <= assignmentTwo_Two)
+        {
+            // Console.WriteLine("Overlap2");
+            numOverlappingPairs += 1;
+        }
+        else if (assignmentTwo_One >= assignmentOne_One && assignmentTwo_One <= assignmentOne_Two)
+        {
+            // Console.WriteLine("Overlap3");
+            numOverlappingPairs += 1;
+        }
+
+        // Console.ReadLine();
+    }
+
+    Console.WriteLine(numOverlappingPairs);
+}
+
+part2();
