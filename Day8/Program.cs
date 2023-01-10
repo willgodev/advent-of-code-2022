401aeec66d4c5c686cd99ebc4cac82e1f3b408e9
wgoins6 <wgoins6@gatech.edu>
2023-01-09 20:11:14 -0500

    Day 8 - Set up additional data structure to do BFS/DFS for visibility

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,9 +1,12 @@
-﻿string[] fileContents = System.IO.File.ReadAllLines("input.txt");
+﻿// string[] fileContents = System.IO.File.ReadAllLines("input.txt");
+string[] fileContents = System.IO.File.ReadAllLines("smol.txt");
 
 int numRows = fileContents.Length;
 int numCols = fileContents[0].Length;
 int visibleTrees = (numCols * 2) + (2 * (numRows-2));
+Console.WriteLine(visibleTrees);
 double[,] treeHeights = new double[numRows, numCols];
+bool[,] visibleTreesMap = new bool[numRows, numCols];
 
 int row = 0;
 foreach (string line in fileContents)
@@ -22,6 +25,30 @@ for (int i = 0; i < numRows; i++)
     for (int j = 0; j < numCols; j++)
     {
         Console.Write(treeHeights[i, j]);
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < numRows; i++)
+{
+    for (int j = 0; j < numCols; j++)
+    {
+        if (i == 0 || i == numRows-1)
+        {
+            visibleTreesMap[i, j] = true;
+        }
+        else if (j == 0 || j == numCols-1)
+        {
+            visibleTreesMap[i, j] = true;
+        }
+    }
+}
+
+for (int i = 0; i < numRows; i++)
+{
+    for (int j = 0; j < numCols; j++)
+    {
+        Console.Write(visibleTreesMap[i, j] + ",");
     }
     Console.WriteLine();
 }
