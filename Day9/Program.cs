a12d5ec35b0f33acf8f073f33d1508e5513473cb
wgoins6 <wgoins6@gatech.edu>
2023-01-13 21:10:00 -0500

    Day 9 - Add grid visualization and relevant points

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,4 +1,32 @@
-﻿string[] fileContents = System.IO.File.ReadAllLines("input.txt");
+﻿static void printGrid(string[,] grid, (int, int) start, (int, int) head, (int, int) tail)
+{
+    Console.WriteLine();
+    for (int i=0; i < grid.GetLength(0); i++)
+    {
+        for (int j=0; j < grid.GetLength(1); j++)
+        {
+            if (i == start.Item1 && j == start.Item2)
+            {
+                Console.Write("s");
+            }
+            else if (i == head.Item1 && j == head.Item2)
+            {
+                Console.Write("H");
+            }
+            else if (i == tail.Item1 && j == tail.Item2)
+            {
+                Console.Write("T");
+            }
+            else
+            {
+                Console.Write(grid[i, j]);
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+string[] fileContents = System.IO.File.ReadAllLines("input.txt");
 
 int maxSteps = 0;
 foreach (string line in fileContents)
@@ -13,21 +41,22 @@ foreach (string line in fileContents)
 
 Console.WriteLine($"Max Steps: {maxSteps}");
 
-string[,] grid = new string[maxSteps+1, maxSteps+1];
+int gridDimension = maxSteps*2 + 1;
+Console.WriteLine(gridDimension);
+string[,] grid = new string[gridDimension, gridDimension];
 
-for (int i=0; i < maxSteps+1; i++)
+for (int i=0; i < gridDimension; i++)
 {
-    for (int j=0; j < maxSteps+1; j++)
+    for (int j=0; j < gridDimension; j++)
     {
         grid[i, j] = ".";
     }
 }
 
-for (int i=0; i < maxSteps+1; i++)
-{
-    for (int j=0; j < maxSteps+1; j++)
-    {
-        Console.Write(grid[i, j]);
-    }
-    Console.WriteLine();
-}
+int halfMaxSteps = gridDimension / 2;
+Console.WriteLine(halfMaxSteps);
+(int, int) start = (halfMaxSteps, halfMaxSteps);
+(int, int) head = (halfMaxSteps, halfMaxSteps);
+(int, int) tail = (halfMaxSteps, halfMaxSteps);
+
+printGrid(grid, start, head, tail);
